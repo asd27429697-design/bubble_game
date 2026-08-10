8daa5c9d05bb75b564527070ad1d5f1af286fd0e
희성 <asd27429697@gmail.com>
2026-08-10 15:49:20 +0900

    test02 시작

---
+++ src/demo/Person.java
@@ -0,0 +1,19 @@
+package demo;
+
+import lombok.*;
+
+//@Setter
+//@Getter
+//@ToString
+// 단축 버전
+@Data
+///////////////////
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private String name;
+    private Integer age;
+
+
+}
